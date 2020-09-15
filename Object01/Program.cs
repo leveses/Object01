cdf2d471117e45e36e3ea22d8d5d14c4a386148b
leveses <boda.zsombor10@gmail.com>
2020-09-15 09:47:18 +0200

    Első objektum orientált programozás

--- Object01/Program.cs
+++ Object01/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Object01
 {
+    class Ember
+    {
+        public string nev;
+        public int eletkor;
+
+        public string Bemutatkozas()
+        {
+            
+            return nev + ", függő, " + eletkor + " éves";
+            
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Ember pisti = new Ember();
+            Ember eva = new Ember();
+            pisti.nev = "Nagy Pista";
+            pisti.eletkor = 20;
+            eva.nev = "Nagy Éva";
+            eva.eletkor = 18;
+
+            
+            pisti.eletkor++;
+
+            Console.WriteLine(pisti.Bemutatkozas());
+
+            Console.ReadKey();
 
         }
     }
